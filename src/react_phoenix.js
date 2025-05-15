cca8e2b4eed195a59d985f4f1d453a8a1e1984a7
pal-alex <palchikovskiy.alexander@gmail.com>
2025-05-15 16:01:28 +0200

    upgrade to React 18

--- src/react_phoenix.js
+++ src/react_phoenix.js
@@ -1,20 +1,27 @@
 import React from "react"
-import ReactDOM from "react-dom"
+import ReactDOM from "react-dom/client"
 
 export default class ReactPhoenix {
   static init() {
     const elements = document.querySelectorAll('[data-react-class]')
     Array.prototype.forEach.call(elements, e => {
       const targetId = document.getElementById(e.dataset.reactTargetId)
-      const targetDiv = targetId ? targetId : e
+      const target = targetId ? targetId : e
       const reactProps = e.dataset.reactProps ? e.dataset.reactProps : "{}"
-      const reactClass = Array.prototype.reduce.call(e.dataset.reactClass.split('.'), (acc, el) => { return acc[el] }, window);
+      const reactClass = Array.prototype.reduce.call(
+        e.dataset.reactClass.split('.'),
+        (acc, el) => acc[el],
+        window
+      )
       const reactElement = React.createElement(reactClass, JSON.parse(reactProps))
-      ReactDOM.render(reactElement, targetDiv)
+      if (!target._reactRoot) {
+        target._reactRoot = ReactDOM.createRoot(target);
+      }
+      target._reactRoot.render(reactElement);
     })
   }
 }
 
-document.addEventListener("DOMContentLoaded", e => {
+document.addEventListener("DOMContentLoaded", () => {
   ReactPhoenix.init()
 })
